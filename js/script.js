7d3da70c2d997ab42a6b93a988403d1a7d81e35d
sashaleal <sashaleal@Sashas-MacBook-Air.local>
2022-07-12 15:18:48 -0500

    adding

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//variable for “overview” div, profile information
+overviewProfile = document.querySelector(".overview")
+//Create variable username, GitHub username as the value
+username = sashaleal
+
+
+//async function
+async function {
+ const res = fetch await https://api.github.com
+ const answer = res.json
+}
+//placeholder
+users/${username}
+
+ <figure>
+ <img alt="user avatar" src=${} />
+</figure>
+<div>
+ <p><strong>Name:</strong> ${}</p>
+ <p><strong>Bio:</strong> ${}</p>
+ <p><strong>Location:</strong> ${}</p>
+ <p><strong>Number of public repos:</strong> ${}</p>
+</div> 
