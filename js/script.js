e342bb2ca4bb79b5a815987b598dd79b2baa609d
sashaleal <sashaleal@Sashas-MacBook-Air.local>
2022-07-12 17:36:33 -0500

    step 2

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
-//variable for “overview” div, profile information
-overviewProfile = document.querySelector(".overview")
-//Create variable username, GitHub username as the value
-username = sashaleal
+//variable “overview” div, profile information
+const overview = document.querySelector(".overview");
+//variable "username", GitHub username as the value
+username = "sashaleal";
 
 
-//async function
-async function {
- const res = fetch await https://api.github.com
- const answer = res.json
-}
-//placeholder
-users/${username}
+//async function to retrieve users info
+const gitUserInfo = async function () {
+ const userInfo = await fetch (`https://api.github.com/users/${username}`);
+ const data = await userInfo.json();
+ displayUserInfo(data);
+};
+//call the async function
+gitUserInfo();
 
+
+//new function to display the fetched user information
+const displayUserInfo = function (data) {
+const div = document.createElement("div");
+div.classList.add("user-info");
+div.innerHTML=`
  <figure>
- <img alt="user avatar" src=${} />
+ <img alt="user avatar" src=${data.avatar_url} />
 </figure>
 <div>
- <p><strong>Name:</strong> ${}</p>
- <p><strong>Bio:</strong> ${}</p>
- <p><strong>Location:</strong> ${}</p>
- <p><strong>Number of public repos:</strong> ${}</p>
-</div> 
+ <p><strong>Name:</strong> ${data.name}</p>
+ <p><strong>Bio:</strong> ${data.bio}</p>
+ <p><strong>Location:</strong> ${data.location}</p>
+ <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+</div>
+`;
+overview.append(div)
+};
